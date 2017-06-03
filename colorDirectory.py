65b4c05126898322e8ddc4038e5f21043886f844
Bhaumik Mistry <Bhaumikpmistry@gmail.com>
2017-06-03 18:08:51 -0400

    bm:- updating the code
    The project has new file related to colors. commit before moving to
    pycharm

---
+++ colorDirectory.py
@@ -0,0 +1,37 @@
+##
+#		notes:		A dictonary for the colors 
+#					and the pixel imformation
+#
+#		modified:	bm	03 06 17	created
+#						created the funtion to get the color dictionary
+#
+############- LEGO list of colors -##############
+# family		color    			R 	G 	B 	#
+# Grey 											#
+# 			Dark stone Grey			91	103	112	#
+# 			medium stone Grey		162	170	173	#
+# Black											#
+# 			Black					39	37	31	#
+# Green											#
+# 			Dark Green				0	132	61	#
+# Yellow 										#
+# 			Bright Yellow			255	205	0	#
+# Reddish Brown									#
+# 			Reddish Brown			122	62	58	#
+# 			Brick Yellow			211	188	141	#
+# Blue											#
+# 			Bright Blue				0	61	165	#
+# 			Earth Blue				0	56	101	#
+# White		White								#
+# Bright Orange  								#
+# 			Bright Orange			255	130	0	#
+# Lilac											#
+# 			Medium Lavendel			160	94	181	#
+# Red 											#
+# 			Bright Red 				239	51	64	#
+#################################################
+
+
+def getColorList():
+	colors = ['Dark stone grey', 'Medium stone grey', 'Black', 'Dark Green', 'Bright yellow', 'Reddish Brown', 'Brick Yellow', 'Bright Blue', 'Earth Blue', 'White', 'Bright Orange', 'Medium Lavender']
+    return colors
