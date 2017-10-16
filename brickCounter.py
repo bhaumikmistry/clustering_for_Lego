1b1ab6a8183570e47eca36f24233697e5dc15ca2
nirzari <niyer@redhat.com>
2017-10-15 20:38:58 -0400

    Enhancing brickCounter coding style

--- brickCounter.py
+++ brickCounter.py
@@ -1,24 +1,31 @@
+# notes: Brick counter
+# modified: 04 04 17 08:56 Bhaumik mistry
 
-#		notes:		Brick counter
-#
-#		modified:	04 04 17 08:56 Bhaumik mistry 
-#
-
-import cv2
 import colorPicker
 
 
 def displayColorInfo():
-	""" This method is for the available colors
-	and color selection  """
-	# for coloe list use this
-	print " List of available colors"
-	colorList = colorPicker.getColorAndValue()
-	#print colorList
+        """ This method is for the available colors
+        and color selection
 
-def testxx(image,gray_image,pixValArray):
-	"""	TO DO RENAME THE FILE	"""
-	#displayColorInfo()
-	(h,w,rgb) = image.shape[:3]
-	for pix in pixValArray:
-		text = pix
+        Return: colorList(data_type): Some desc
+        """
+        # for color list use this
+        print ('List of available colors')
+        colorList = colorPicker.getColorAndValue()
+        # print colorList
+        return colorList
+
+
+def testxx(image, gray_image, pixValArray):
+        """ Some Description of the method
+        Args:
+            image(data_type): Some desc
+            gray_image(data_type): Some desc
+            pixValArray(data_type): Some desc
+        """
+        # TODO: RENAME the file
+        # displayColorInfo()
+        (h, w, rgb) = image.shape[:3]
+        for pix in pixValArray:
+            text = pix
